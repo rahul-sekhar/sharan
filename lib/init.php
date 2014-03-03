5fc310ea66ad167514c6bde77db1abfb6b3e918a
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-03-03 22:09:57 +0530

    page sidebar base

--- lib/init.php
+++ lib/init.php
@@ -5,7 +5,7 @@
 function roots_setup() {
   // Add post thumbnails (http://codex.wordpress.org/Post_Thumbnails)
   add_theme_support('post-thumbnails');
-  // set_post_thumbnail_size(150, 150, false);
+  set_post_thumbnail_size(225, 0, false);
   // add_image_size('category-thumb', 300, 9999); // 300px wide (and unlimited height)
 
   // Tell the TinyMCE editor to use a custom stylesheet
---
+++ templates/sidebar-page.php
@@ -0,0 +1,7 @@
+<aside class="page-sidebar">
+  <?php
+  if (has_post_thumbnail()) :
+    the_post_thumbnail();
+  endif;
+  ?>
+</aside>
