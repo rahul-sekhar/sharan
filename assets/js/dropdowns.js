be3ce4f390d1c18900da511690c0d4dda936aaaf
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-03-05 09:12:37 +0530

    dropdown bug

--- assets/js/dropdowns.js
+++ assets/js/dropdowns.js
@@ -4,10 +4,12 @@ jQuery(function ($) {
 
   console.log(containers.find('.dropdown-title'));
 
-  containers.find('.dropdown-title').nextAll().wrapAll('<div class="dropdown-content"></div>');
+  containers.find('.dropdown-title').each(function () {
+    $(this).nextUntil('.dropdown-title').wrapAll('<div class="dropdown-content"></div>');
+  });
   containers.find('.dropdown-content').hide();
 
   containers.on('click', '.dropdown-title', function (e) {
-    $(this).closest('.dropdown-container').find('.dropdown-content').slideToggle();
+    $(this).next('.dropdown-content').slideToggle();
   });
 });
