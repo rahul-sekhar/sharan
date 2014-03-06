26c08d4b070e34fa138669a82756df4f3b6d597f
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-03-06 17:29:05 +0530

    nav changes

--- templates/navigation-page.php
+++ templates/navigation-page.php
@@ -3,17 +3,30 @@
   <?php if ($page_nav) : ?>
     <h3><?php echo $page_nav['name']; ?></h3>
 
-    <?php if ($page_nav['sections']) : foreach ($page_nav['sections'] as $section) : ?>
-      <ul>
-        <li class="title"><?php echo $section['name']; ?></li>
+    <?php if ($page_nav['sections']) : $first = true  ?>
+      <?php foreach ($page_nav['sections'] as $section) : ?>
+        <?php
+        if ($first) :
+          $first = false;
+        ?>
+          <ul>
+        <?php elseif ($section['name']) : ?>
+          </ul>
+          <ul>
+        <?php endif; ?>
+
+        <?php if ($section['name']) : ?>
+          <li class="title"><?php echo $section['name']; ?></li>
+        <?php endif; ?>
 
         <?php if ($section['links']) : foreach ($section['links'] as $link) : ?>
           <li>
             <a href="<?php echo $link['url']; ?>"><?php echo $link['name']; ?></a>
           </li>
         <?php endforeach; endif; ?>
+      <?php endforeach; ?>
       </ul>
-    <?php endforeach; endif; ?>
+    <?php endif; ?>
 
   <?php endif; ?>
 </nav>
