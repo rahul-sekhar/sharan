728a88f76818513374e919f323c769324ed7dcaa
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-05-21 12:13:49 +0530

    resources page base

--- resources-page-template.php
+++ resources-page-template.php
@@ -3,67 +3,55 @@
 Template Name: Resources page
 */
 ?>
-<?php while (have_posts()) : the_post(); ?>
-  <div class="page-container">
-    <?php get_template_part('templates/navigation', 'page'); ?>
+<div class="page-container" id="resources">
+  <?php get_template_part('templates/sidebar', 'page'); ?>
 
-    <?php get_template_part('templates/sidebar', 'page'); ?>
+  <div class="page content">
+    <?php get_template_part('templates/content', 'page'); ?>
 
-    <div class="page content">
-      <?php get_template_part('templates/content', 'page'); ?>
+    <?php
+    $args = array(
+      'posts_per_page' => -1,
+      'post_type' => 'links',
+    );
+    $links = new WP_Query($args);
 
-      <?php
-      if ( have_rows('categories') ) :
-      ?>
-        <div class="dropdown-container">
+    if ($links->have_posts()) :
+    ?>
+    <ul class="links">
+      <?php while($links->have_posts()) : $links->the_post(); ?>
+        <li>
+          <p class="name"><?php the_field('name') ?></p>
+          <p class="link"><a href="<?php the_field('link') ?>">Link</a></p>
+          <div class="description"><?php the_field('description') ?></div>
+        </li>
+      <?php endwhile; ?>
+    </ul>
+    <?php
+    endif;
+    wp_reset_postdata();
+    ?>
 
-        <?php while ( have_rows('categories') ) : the_row(); ?>
-          <h5 class="dropdown-title"><?php the_sub_field('name'); ?></h5>
+    <?php
+    $args = array(
+      'posts_per_page' => -1,
+      'post_type' => 'books',
+    );
+    $books = new WP_Query($args);
 
-          <?php if ( have_rows('books') ) : ?>
-            <h6>Books</h6>
-            <ul class="books">
-              <?php while ( have_rows('books') ) : the_row() ?>
-                <li>
-                  <?php
-                  $image = get_sub_field('image');
-                  if ($image) : ?>
-                    <img src="<?php echo $image['sizes']['book']; ?>" alt="" />
-                  <?php endif; ?>
-
-                  <p class="name"><?php the_sub_field('name') ?></p>
-                  <p class="author"><?php the_sub_field('author') ?></p>
-                  <p class="buy"><a href="<?php the_sub_field('buy-link'); ?>">Buy</a></p>
-                  <div class="description">
-                    <?php the_sub_field('description'); ?>
-                  </div>
-                </li>
-              <?php endwhile; ?>
-            </ul>
-          <?php
-          endif;
-
-          if ( have_rows('links') ) : ?>
-            <h6>Links</h6>
-            <ul class="links">
-              <?php while ( have_rows('links') ) : the_row() ?>
-                <li>
-                  <p class="name"><?php the_sub_field('name') ?></p>
-                  <p class="link"><a href="<?php the_sub_field('link'); ?>">Link</a></p>
-                  <div class="description">
-                    <?php the_sub_field('description'); ?>
-                  </div>
-                </li>
-              <?php endwhile; ?>
-            </ul>
-          <?php
-          endif;
-        endwhile; ?>
-
-        </div>
-      <?php
-      endif;
-      ?>
-    </div><!-- /.page-content -->
-  </div>
-<?php endwhile; ?>
+    if ($books->have_posts()) :
+    ?>
+    <ul class="books">
+      <?php while($books->have_posts()) : $books->the_post(); ?>
+        <li>
+          <?php $image = get_field('cover'); ?>
+          <img src="<?php echo $image['sizes']['book']; ?>" alt="" />
+        </li>
+      <?php endwhile; ?>
+    </ul>
+    <?php
+    endif;
+    wp_reset_postdata();
+    ?>
+  </div><!-- /.page-content -->
+</div>
