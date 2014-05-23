097aabec761a7b95ddf56d17b5ddbbb8f4247ad1
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-05-24 00:15:31 +0530

    remove side nav for pages that don't have it

--- page.php
+++ page.php
@@ -1,5 +1,5 @@
 <?php while (have_posts()) : the_post(); ?>
-  <div class="page inner-container with-side-nav">
+  <div class="page inner-container<?php if (sharan_get_page_nav()) echo ' with-side-nav'; ?>">
     <?php get_template_part('templates/side-nav', 'page'); ?>
 
     <?php get_template_part('templates/sidebar', 'page'); ?>
