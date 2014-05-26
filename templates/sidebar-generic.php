12339bbfbfe96716ebc6678e7c93e50b8052c7a4
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-05-26 21:21:51 +0530

    default sidebar fix

--- templates/sidebar-generic.php
+++ templates/sidebar-generic.php
@@ -1,13 +1,9 @@
 <aside class="sidebar">
   <?php
-  if (has_post_thumbnail()) :
-    the_post_thumbnail();
-  else :
-    $default_image = get_field('default_sidebar_image', 'options');
+  $default_image = get_field('default_sidebar_image', 'options');
   ?>
     <img src="<?php echo $default_image['sizes']['post-thumbnail'] ?>" alt="" />
   <?php
-  endif;
 
   get_template_part('templates/newsletter-box');
 
