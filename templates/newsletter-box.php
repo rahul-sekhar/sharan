d6950c916406b83c7b5c20a7828fa2f1aaeec48d
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-06-02 19:09:28 +0530

    content and styling fixes

--- templates/newsletter-box.php
+++ templates/newsletter-box.php
@@ -1,9 +1,9 @@
 <form id="subscribe-form">
-  <label for="subscribe-email">Subscribe to our newsletter</label>
+  <label for="subscribe-email">Join our mailing list</label>
   <div class="subscribe-email-box">
     <input id="subscribe-email" type="email" name="email" placeholder="Enter your email address" />
     <button type="submit"><i class="icon-ok"></i></button>
   </div>
 
-  <a href="<?php the_field('newsletter_link', 'options'); ?>" class="archive">View archive</a>
+  <a href="<?php the_field('newsletter_link', 'options'); ?>" class="archive">Newsletter archive</a>
 </form>
