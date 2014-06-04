d1e4500b2d225dcf729801b1a53418d9ae876785
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-06-04 12:56:40 +0530

    subscription button

--- templates/newsletter-box.php
+++ templates/newsletter-box.php
@@ -2,7 +2,7 @@
   <label for="subscribe-email">Join our mailing list</label>
   <div class="subscribe-email-box">
     <input id="subscribe-email" type="email" name="email" placeholder="Enter your email address" />
-    <button type="submit"><i class="icon-ok"></i></button>
+    <button type="submit"><i class="icon-right-open-big"></i></button>
   </div>
 
   <a href="<?php the_field('newsletter_link', 'options'); ?>" class="archive">Newsletter archive</a>
