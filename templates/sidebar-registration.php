353e727c1e742fe05c3fc9d7ccca6230c3d6f458
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-07-07 11:58:56 +0530

    sharan sidebar

--- templates/sidebar-registration.php
+++ templates/sidebar-registration.php
@@ -1,5 +1,7 @@
-<aside class="registration form sidebar">
-  <?php
-  the_field('registration_faq', 'options');
-  ?>
-</aside>
+<?php if (get_field('registration_faq', 'options')) : ?>
+  <aside class="registration form sidebar">
+    <?php
+    the_field('registration_faq', 'options');
+    ?>
+  </aside>
+<?php endif; ?>
--- templates/sidebar-subscription.php
+++ templates/sidebar-subscription.php
@@ -1,5 +1,7 @@
-<aside class="subscription form sidebar">
-  <?php
-  the_field('subscription_faq', 'options');
-  ?>
-</aside>
+<?php if (get_field('subscription_faq', 'options')) : ?>
+  <aside class="subscription form sidebar">
+    <?php
+    the_field('subscription_faq', 'options');
+    ?>
+  </aside>
+<?php endif; ?>
