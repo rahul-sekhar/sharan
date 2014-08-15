e97eca000a2cb12d5634b5d947756ff574063a9b
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-08-15 11:04:18 +0530

    rss feed

--- templates/social-box.php
+++ templates/social-box.php
@@ -1,12 +1,12 @@
 <?php
 $links = get_field('social_links', 'options');
-if ($links): ?>
+?>
 <div class="social">
   <p>Follow SHARAN</p>
   <ul>
     <?php foreach ($links as $link): ?>
       <li><a class="icon-<?php echo $link['type']; ?>" href="<?php echo $link['url']; ?>"></a></li>
     <?php endforeach; ?>
+    <li><a class="icon-rss" href="<?php bloginfo('rss2_url'); ?>"></a></li>
   </ul>
 </div>
-<?php endif; ?>
