5fc5b253535daf31e51f5c82a85b0a80c8012fbc
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-08-15 15:08:35 +0530

    add events to feed

--- templates/social-box.php
+++ templates/social-box.php
@@ -7,6 +7,6 @@ $links = get_field('social_links', 'options');
     <?php foreach ($links as $link): ?>
       <li><a class="icon-<?php echo $link['type']; ?>" href="<?php echo $link['url']; ?>"></a></li>
     <?php endforeach; ?>
-    <li><a class="icon-rss" href="<?php bloginfo('rss2_url'); ?>"></a></li>
+    <li><a class="icon-rss" href="/feed/?post_type[]=post&amp;post_type[]=events"></a></li>
   </ul>
 </div>
