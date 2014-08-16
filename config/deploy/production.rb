8468a0b1410c9f48a2afff9085fc2377a7787b59
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-08-16 11:10:48 +0530

    remove capistrano, update readme

--- config/deploy/production.rb
+++
@@ -1,7 +0,0 @@
-set :user, "sharan"
-set :use_sudo, false
-# set :deploy_to, "/home/#{user}/sharan.kairi.in/deploys/sharan"
-set :uploads_path, "/home/#{user}/site/wp-content/uploads"
-set :host_name, "sharan-india.org"
-
-server host_name, :app, :web, :db, :primary => true
--- config/deploy/staging.rb
+++
@@ -1,7 +0,0 @@
-set :user, "sharanwp"
-set :use_sudo, false
-set :deploy_to, "/home/#{user}/sharan.kairi.in/deploys/sharan"
-set :uploads_path, "/home/#{user}/sharan.kairi.in/wp-content/uploads"
-set :host_name, "sharan.kairi.in"
-
-server host_name, :app, :web, :db, :primary => true
